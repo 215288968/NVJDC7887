ccaa61ab530c3ea6630b996e94a18d3c53bc45c1
215288968 <79185259+215288968@users.noreply.github.com>
2021-10-17 22:40:37 +0800

    Delete decrypt_secret.sh

--- decrypt_secret.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$USER_PASS" \
---output frpc.ini frpc.ini.gpg
